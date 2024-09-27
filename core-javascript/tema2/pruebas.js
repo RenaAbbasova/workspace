916bce36a0d39b4752b6722b4dd07dc139cdca09
RenaAbbasova <abbasova.r@gmail.com>
2024-09-27 08:52:22 +0200

    evaluacion3

---
+++ core-javascript/tema2/pruebas.js
@@ -0,0 +1,18 @@
+function sumar(sum1, sum2) {
+    return sum1 + sum2;
+}
+
+let soluction = sumar(5, 3);
+console.log(soluction);
+
+
+function larg(num1, num2) {
+    if (num1 > num2) {
+        return num1
+    } else {
+        return num2
+    }
+
+}
+s = larg(1,2)
+console.log(s)
