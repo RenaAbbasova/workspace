e0e310c15cd02dc0d9dffe7c469d1b744d4c3b21
RenaAbbasova <abbasova.r@gmail.com>
2024-09-27 19:54:57 +0200

    no changes

---
+++ core-javascript/modules_exercices/exer4.js
@@ -0,0 +1,47 @@
+/* for (let i = 1; i <=100; i++) {
+    alert(i);
+} */
+
+
+/* for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+} 
+} */
+
+/* for (let i = 20; i <= 1000; i += 20) {
+    console.log(i);
+} */
+
+/* function square(size) {
+    for(let i = 0; i < size; i++) {
+        let row = "";
+        for (let k = 0; k < size; k++) {
+            row += "# ";
+        }
+        console.log(row);
+    }
+}
+square(5) */
+
+/* function multiplicationtaable() {
+    for (let i = 0; i <= 9; i++) {
+        console.log(`tabla de ${i}:`)
+        for (let k = 0; k <= 9; k++) {
+            console.log(`${i} x ${k} = ${i * k}`)
+        }
+        console.log('')
+
+    }
+}
+multiplicationtaable(); */
+
+
+function square(size) {
+    for(let i = 0; i < size; i++) {
+        let row = "";
+        for (let k = 0; k < size; k++) {
+            row += "# ";
+        }
+        console.log(row);
+    } 
--- core-javascript/modules_exercices/exercices3.js
+++ core-javascript/modules_exercices/exercices3.js
@@ -1,20 +1,47 @@
-user = " Diametro de una rueda y grosor"
+// Pedir al usuario el diámetro y grosor de la rueda
+let diameter = parseFloat(prompt("Introduce el diámetro de la rueda en metros:"));
+let grosor = parseFloat(prompt("Introduce el grosor de la rueda en metros:"));
 
-let diameter 
-let diameter 
+// validar si los datos son válidos
+if (isNaN(diameter) || isNaN(grosor)) {
+    console.log("Por favor, introduce valores numéricos validos.")
+} else { 
+    // Condición para el diámetro de la rueda
+    if (diameter > 1.4) {
+        console.log("La rueda es para un vehículo grande");
+    } else if (diameter <= 1.4 && diameter > 0.8) {
+        console.log("La rueda es para un vehículo mediano");
+    } else {
+        console.log("La rueda es para un vehículo pequeño");
+    }       
 
-if (diameter > 1.4)
-    console.log("La rueda es para un vehiculo grande")
-else if (diametro <= 1.4) {
-    console.log("La rueda es para un vehiculo mediano")
-} else {
-    return "La rueda es para un vehiculo pequeño"
+    // Condiciones para el grosor
+    if ((diameter > 1.4 && grosor < 0.4) || (diameter <= 1.4 && diameter > 0.8 && grosor < 0.25)) {
+    console.log("El grosor para esta rueda es inferior al recomendado");
+    }
+
 }
 
-else if (diameter > 1.4) || (grosor < 0.4) && (diameter <= 1.4) || (diameter > 0.8) || (grosor < 0.25) {
-    console.log("El grosor para esta rueda es inferior al recomendado")
-
-}
 
 
-username = window.prompt(" whatb is your user name?")
+
+
+
+
+
+/* 
+username = window.prompt(" whatb is your user name?") */
+/* 
+let age = 25;
+let price = 85;
+console.log(`My age is ${age}`);
+console.log(`The coat cost ${price}`);
+
+let username;
+username = window.prompt("la rueda es para un vehiculo grande");
+console.log(username)
+ */ 
+
+
+
+
