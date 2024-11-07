55d70064d0c06a051b4e7fef38b6ca6041cd1ce8
RenaAbbasova <abbasova.r@gmail.com>
2024-11-07 09:02:54 +0100

    no changes

--- html/Artist_web/js/main.js
+++ html/Artist_web/js/main.js
@@ -3,3 +3,41 @@ const toggleLightDark = () => {
     const body = document.querySelector('body');
     body.classList.toggle('light');
 };
+
+// actors page
+let currentPage = 1;
+const totalPages = 10;
+
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        updatePaginationButtons();
+        // Logic to display the previous page content
+    }
+}
+
+function nextPage() {
+    if (currentPage < totalPages) {
+        currentPage++;
+        updatePaginationButtons();
+        // Logic to display the next page content
+    }
+}
+
+function updatePaginationButtons() {
+    document.getElementById("prevBtn").disabled = currentPage === 1;
+    document.getElementById("nextBtn").disabled = currentPage === totalPages;
+}
+
+updatePaginationButtons(); // Initialize button states
+
+// acter page
+function toggleMoreInfo(actorId) {
+    const moreInfoContent = document.getElementById(actorId);
+    if (moreInfoContent.style.display === "none" || moreInfoContent.style.display === "") {
+        moreInfoContent.style.display = "block"; // Show more info
+    } else {
+        moreInfoContent.style.display = "none"; // Hide more info
+    }
+}
+
